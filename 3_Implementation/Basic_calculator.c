c57b791a4d86996928d9a31dea70f397fecfd091
Sanket22Kamble <guddukamble22032000@gmail.com>
2022-02-10 22:52:39 +0530

    commit

--- 3_Implementation/Basic_calculator.c
+++ 3_Implementation/Basic_calculator.c
@@ -1,5 +1,16 @@
+/**
+ * @file Basic_calculator.c
+ * @author Sanket Kamble (guddukamble22032000@gmail.com)
+ * @brief Program for Understanding the functions of
+ * @version 0.1
+ * @date 2022-02-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include<stdio.h>
 int main()
 {
-    printf("Hello");
+    printf("hello\n");
 }
